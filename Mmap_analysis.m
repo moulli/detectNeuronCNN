9fcd3853744e24a973d44677a25d44706841a8fe
Moulli <hippolyte.moulle@upmc.fr>
2019-04-15 17:47:45 +0200

    Added Mmap analysis to recognize neurons based on gaussians

--- Mmap_analysis.m
+++ Mmap_analysis.m
@@ -41,91 +41,122 @@ end
 
 %% Algorithm discovering map:
 
-figure('units', 'normalized', 'outerposition', [0 0 1 1])
-subplot(1, 2, 1)
-image(mlayertemp, 'CDataMapping', 'scaled')
-colorbar
-
-mlayer_ind = ones(slayer);
-mneurons = zeros([slayer, 0]);
-while ~isequal(mlayer_ind, zeros(slayer))
-    
-    % Chosing which action to do:
-    nleft = find(mlayer_ind == -1);
-    if isempty(nleft)
-        % Adding new neuron:
-        mat_temp = mlayer_ind .* mlayertemp;
-        [~, next_pt] = max(mat_temp(:));
-        [xpt, ypt] = ind2sub(slayer, next_pt);
-        mneurons = cat(3, mneurons, zeros(slayer));
-    else
-        % Adding waiting neuron:
-        [xpt, ypt] = ind2sub(slayer, nleft(1));
-    end
-    % Changing parameters:
-    mneurons(xpt, ypt, end) = 1;
-    % Setting indication to 0:
-    mlayer_ind(xpt, ypt) = 0;
-    
-    % Adding waiting points:
-    % In x-axis direction:
-    if xpt == 1
-        neix = xpt + 1; 
-    elseif xpt == slayer(1)
-        neix = xpt - 1;
-    else 
-        neix = [xpt-1; xpt+1];
-    end
-    % In y-axis direction:
-    if ypt == 1
-        neiy = ypt + 1;
-    elseif ypt == slayer(2)
-        neiy = ypt - 1;
-    else
-        neiy = [ypt-1; ypt+1];
-    end
-    % Forming final couples:
-    nex = zeros(length(neix)*length(neiy), 1);
-    ney = zeros(length(neix)*length(neiy), 1);
-    for i = 1:length(neix)
-        for j = 1:length(neiy)
-            nex(length(neiy)*(i-1)+j) = neix(i);
-            ney(length(neiy)*(i-1)+j) = neiy(j);
-        end
-    end
-    % Analyzing each point independently:
-    for k = 1:(length(neix)*length(neiy))
-        if mlayer_ind(nex(k), ney(k)) ~= 0 && mlayertemp(nex(k), ney(k)) < mlayertemp(xpt, ypt)
-            mlayer_ind(nex(k), ney(k)) = -1;
-        end
-    end
-    
-    % Plotting:
-    pause(0.05)
-    subplot(1, 2, 2)
-    image(mlayertemp .* 2 .* mlayer_ind(:, :, end), 'CDataMapping', 'scaled')
-end
+% figure('units', 'normalized', 'outerposition', [0 0 1 1])
+% subplot(1, 2, 1)
+% image(mlayertemp, 'CDataMapping', 'scaled')
+% colorbar
+% 
+% mlayer_ind = ones(slayer);
+% mneurons = zeros([slayer, 0]);
+% while ~isequal(mlayer_ind, zeros(slayer))
+%     
+%     % Chosing which action to do:
+%     nleft = find(mlayer_ind == -1);
+%     if isempty(nleft)
+%         % Adding new neuron:
+%         mat_temp = mlayer_ind .* mlayertemp;
+%         [~, next_pt] = max(mat_temp(:));
+%         [xpt, ypt] = ind2sub(slayer, next_pt);
+%         mneurons = cat(3, mneurons, zeros(slayer));
+%     else
+%         % Adding waiting neuron:
+%         [xpt, ypt] = ind2sub(slayer, nleft(1));
+%     end
+%     % Changing parameters:
+%     mneurons(xpt, ypt, end) = 1;
+%     % Setting indication to 0:
+%     mlayer_ind(xpt, ypt) = 0;
+%     
+%     % Adding waiting points:
+%     % In x-axis direction:
+%     if xpt == 1
+%         neix = xpt + 1; 
+%     elseif xpt == slayer(1)
+%         neix = xpt - 1;
+%     else 
+%         neix = [xpt-1; xpt+1];
+%     end
+%     % In y-axis direction:
+%     if ypt == 1
+%         neiy = ypt + 1;
+%     elseif ypt == slayer(2)
+%         neiy = ypt - 1;
+%     else
+%         neiy = [ypt-1; ypt+1];
+%     end
+%     % Forming final couples:
+%     nex = zeros(length(neix)*length(neiy), 1);
+%     ney = zeros(length(neix)*length(neiy), 1);
+%     for i = 1:length(neix)
+%         for j = 1:length(neiy)
+%             nex(length(neiy)*(i-1)+j) = neix(i);
+%             ney(length(neiy)*(i-1)+j) = neiy(j);
+%         end
+%     end
+%     % Analyzing each point independently:
+%     for k = 1:(length(neix)*length(neiy))
+%         if mlayer_ind(nex(k), ney(k)) ~= 0 && mlayertemp(nex(k), ney(k)) < mlayertemp(xpt, ypt)
+%             mlayer_ind(nex(k), ney(k)) = -1;
+%         end
+%     end
+%     
+% %     % Plotting:
+% %     pause(0.05)
+% %     subplot(1, 2, 2)
+% %     image(mlayertemp .* 2 .* mlayer_ind(:, :, end), 'CDataMapping', 'scaled')
+% end
 
 
 %% Works really badly, let's try with gaussians:
 
+mlayer_gauss = double(mlayer);
+figure; image(mlayer_gauss, 'CDataMapping', 'scaled'); colorbar; axis equal
+slayer = size(mlayer_gauss);
 mlayer_ind = ones(slayer);
-mneurons = zeros([slayer, 10]);
-for i = 1:10
-    [maxmax, maxind] = max(mlayertemp(:));
+% mneurons = zeros([slayer, 0]);
+mneurons = {};
+[Ylay, Xlay] = meshgrid(1:slayer(2), 1:slayer(1));
+Clay = [Xlay(:), Ylay(:)];
+print_info = 0;
+tic
+while 1
+    [maxmax, maxind] = max(mlayer_gauss(:));
+    if maxmax < 900
+        break
+    end
     [xpt, ypt] = ind2sub(slayer, maxind);
     pts = zeros(0, 2);
-    for k1 = -2:2
-        for k2 = -2:2
-            pts = [pts; xpt+k1, ypt+k2];
+    for k1 = -4:4
+        for k2 = -4:4
+            xtemp = max(1, xpt+k1); xtemp = min(slayer(1), xtemp);
+            ytemp = max(1, ypt+k2); ytemp = min(slayer(2), ytemp);
+            pts_temp = repmat([xtemp, ytemp], round(mlayer(xtemp, ytemp)), 1);
+            pts = [pts; pts_temp];
         end
     end
     GMMtemp = fitgmdist(pts, 1);
     mu = GMMtemp.mu;
     sigma = GMMtemp.Sigma;
     A = maxmax * sqrt((2*pi)^2*det(sigma));
-    
+    % Computing gaussian:
+    gauss_temp = A * mvnpdf(unique(pts, 'rows'), mu, sigma);
+    minus_gauss = reshape(A * mvnpdf(Clay, mu, sigma), slayer);
+    mlayer_gauss = mlayer_gauss - minus_gauss;
+    % Adding neuron:
+    minus_gauss(minus_gauss < 0.001) = 0;
+%     mneurons = cat(3, mneurons, minus_gauss);
+    mneurons = [mneurons; {sparse(minus_gauss)}];
+    % Information:
+    if mod(length(mneurons), 100) == 0
+        fprintf(repmat('\b', 1, print_info));
+        print_info = fprintf('%.0f neurons already found in %.3f seconds \n', [length(mneurons), toc]);
+    end
 end
+temp = zeros(slayer);
+for i = 1:length(mneurons); temp = temp + full(mneurons{i}); end
+figure; image(temp, 'CDataMapping', 'scaled'); colorbar; axis equal
+path_created = fullfile('/home/ljp/Science/Hippolyte', 'mneurons.mat');
+save(path_created, 'mneurons');
     
 
     
